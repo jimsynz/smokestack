ac2521c3c307f3fa9ea933396c7a5f7f059b121f
James Harton <james@harton.nz>
2023-08-17 19:43:06 +1200

    chore: add missing `git_ops` config.

---
+++ config/config.exs
@@ -0,0 +1,9 @@
+import Config
+
+config :git_ops,
+  mix_project: Mix.Project.get!(),
+  changelog_file: "CHANGELOG.md",
+  repository_url: "https://code.harton.nz/james/smokestack",
+  manage_mix_version?: true,
+  version_tag_prefix: "v",
+  manage_readme_version: "README.md"
